71d4880494c40faec0d375ee9da606bcc13f93e3
gift07-tester <giftmdlalose007@gmail.com>
2024-08-15 14:51:41 +0200

    adding files for exercise 1

--- src/test/java/com/serenitydojo/HelloWorldWriteTest.java
+++ src/test/java/com/serenitydojo/HelloWorldWriteTest.java
@@ -6,4 +6,21 @@ import org.junit.Test;
           HelloWorldWriter writer = new HelloWorldWriter();
           writer.writeHelloWorld();
       }
+      @Test
+      public void declaringnumbers(){
+          final int agethisYear = 42;
+          int agenextYear = agethisYear + 1;
+          System.out.println("age next year = " + agenextYear);
+
+      }
+      @Test
+      public void workingWithStrings() {
+          String firstName = "        sarah-jane   ";
+          String upperCaseFirstName = firstName.replace("sara","mary");
+          System.out.println(upperCaseFirstName.trim());
+
+
+      }
+
+
 }
---
+++ src/test/java/com/serenitydojo/dataTypes/WhenWorkingWithWholeNumbers.java
@@ -0,0 +1,20 @@
+package com.serenitydojo.dataTypes;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class WhenWorkingWithWholeNumbers {
+    @Test
+    public void addingNumbersTogether(){
+    int initialYear = 1985;
+    int targetYear = 0;
+
+    int timeJump = 30;
+    targetYear = initialYear + timeJump;
+    assertThat(targetYear, equalTo(2015));
+
+
+    }
+
+}
